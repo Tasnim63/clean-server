d5c4719d8ef9babeafa1c6a5a2501c573c52cc2e
tasnim63 <tamanna202163@gmail.com>
2022-05-21 11:17:38 +0600

    error

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ run().catch(console.dir);
 app.get('/', async (req, res) => {
     res.send('Hello World!')
 })
-    // zzxbb
+// zzxbb
 
-    / dggfd
+// / dggfd
 // clean server
 app.listen(port, () => {
     console.log(`listening from house cleaner  ${port}`)
