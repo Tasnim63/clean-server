0daebd91c211aa59b26b27fab00833a8a2bfd7a1
tasnim63 <tamanna202163@gmail.com>
2022-05-21 09:54:58 +0600

    clean

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+// middleware
+app.use(cors({
+    origin: "*",
+}));
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t8vkx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("houseCleaner").collection("services");
+        // const userCollection = client.db("houseCleaner").collection("users");
+
+
+        app.get('/service', async (req, res) => {
+
+            const services = await serviceCollection.find({}).toArray();
+            res.send(services)
+        })
+        console.log('db conected');
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', async (req, res) => {
+    res.send('Hello World!')
+})
+// clean server
+app.listen(port, () => {
+    console.log(`listening from house cleaner  ${port}`)
+})
